6ea7e06a9d1367ce83bf7f20347a36a7eda28a79
Vlad Kovaliov <v.v.kovaliov@gmail.com>
2026-07-18 22:06:34 +0300

    feat: adds logout

--- apps/react-wrapper/src/App.tsx
+++ apps/react-wrapper/src/App.tsx
@@ -6,12 +6,15 @@ import "@mantine/spotlight/styles.css";
 import { Outlet, useNavigate } from "react-router-dom";
 
 import {
+  AppShell,
   MantineProvider,
   createTheme,
 } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
 import { Spotlight } from "@mantine/spotlight";
 
 import { SingleTabManagerWrapper } from "./components/single-tab-manager";
+import { AppShellHeader } from "./components/app-shell-header/app-shell-header";
 import { APPS } from "./shared/config/apps";
 import { buildSpotlightActions } from "./features/spotlight";
 
@@ -22,14 +25,21 @@ const theme = createTheme({
 
 export default function App() {
   const navigate = useNavigate();
+  const [opened, { toggle }] = useDisclosure();
 
   const spotlightActions = buildSpotlightActions(APPS, navigate);
 
   return (
     <MantineProvider theme={theme} defaultColorScheme="dark">
-      <SingleTabManagerWrapper>
-        <Outlet />
-      </SingleTabManagerWrapper>
+      <AppShell header={{ height: 60 }}>
+        <AppShellHeader opened={opened} toggle={toggle} />
+
+        <AppShell.Main>
+          <SingleTabManagerWrapper>
+            <Outlet />
+          </SingleTabManagerWrapper>
+        </AppShell.Main>
+      </AppShell>
 
       <Spotlight
         shortcut="mod + K"
